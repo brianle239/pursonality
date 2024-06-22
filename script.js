30dc38ab11aee16454f8ccef459bd2b0ee4c7f9b
Brian Le <brianle239@gmail.com>
2024-06-21 22:21:16 -0700

    update and fix progress bar

--- script.js
+++ script.js
@@ -1,39 +1,62 @@
-async function updateContent() {
-    // Fetch new content (replace with your actual logic)
-    const response = await fetch("./page1/index.html").then(x => x.text());
-    const parser = new DOMParser();
-    console.log(response)
-        // newContent = response
-        // console.log(response)
+let audio_list = []
+let element_main = null
+let page = 0
+let max_page = 3 // 0 index
 
-    // Parse the HTML string
+
+async function nextPage() {
+
+    let page_file = ""
+
+    // Change if adding more pages
+    if (page == 2) {
+        page_file = "./end_page/index.html"
+    } else {
+        page_file = "./page" + (page + 1) + "/index.html"
+    }
+    console.log(page_file)
+    page += 1
+    const response = await fetch(page_file).then(x => x.text());
+    const parser = new DOMParser();
+
+    const newFragment = parser.parseFromString(response, "text/html");
+    const targetElement = document.getElementById("main");
+
+    targetElement.replaceWith(newFragment.getElementById("main"))
+    if (page == 2) {
+        handleProgress(nextPage)
+
+    }
+}
+
+async function prevPage() {
+    let page_file = "";
+    if (page == 1) {
+        page_file = "./index.html";
+    } else if (page == max_page) {
+        page_file = "./page" + (page - 2) + "/index.html";
+        page -= 1
+
+    } else {
+        page_file = "./page" + (page - 1) + "/index.html";
+    }
+
+    page -= 1;
+
+    const response = await fetch(page_file).then(x => x.text());
+    const parser = new DOMParser();
 
     const newFragment = parser.parseFromString(response, "text/html");
     console.log(newFragment)
 
-    // Get the element to replace
-    const targetElement = document.getElementById("main_body");
+    const targetElement = document.getElementById("main");
 
-    // Replace content
+    console.log("new", newFragment.getElementById("main"))
+    targetElement.replaceWith(newFragment.getElementById("main"))
 
-    targetElement.replaceWith(newFragment.body.firstChild)
-        // targetElement.innerHTML = newFragment.body.firstChild; // Get first child (paragraph)
-        // console.log(newFragment.body.firstChild)
-        // console.log(newFragment.innerHTML)
 }
 
 
-
-// const audio2 = document.getElementById("cat_music2")
-// audio2.load()
-// audio2.addEventListener("ended", function() {
-//     playRandomMusic()
-// })
-
-
-
-let audio_list = []
-let element = null
 
 function fadeVolume(audioElement, newVolume, duration = 1000) {
     const startVolume = audioElement.volume;
@@ -54,9 +77,9 @@ function fadeVolume(audioElement, newVolume, duration = 1000) {
 function playRandomMusic() {
     random_int = Math.floor(Math.random() * audio_list.length)
     audio = audio_list[random_int]
-    audio.volume = 0.1;
+    audio.volume = 0.2;
     audio.play();
-    element.removeEventListener("click", playRandomMusic);
+    element_main.removeEventListener("click", playRandomMusic);
     fadeVolume(audio, newVolume = .5, duration = 5000)
 }
 
@@ -94,6 +117,45 @@ window.onload = function() {
     })
     audio_list.push(audio3)
 
-    element = document.getElementById("main");
-    element.addEventListener("click", playRandomMusic);
+    element_main = document.getElementById("main");
+    element_main.addEventListener("click", playRandomMusic);
 }
+
+function changeProgess(bar, i) {
+    bar.style.width = `${(i+1)*20}%`
+}
+
+function handleProgress(callback) {
+    bar_text = ["Gathering Data", "Calling MeowGPT", "Getting IP Address", "Generating Result", "Notifying Angeline and Brian"]
+    let bar = document.getElementById("progress_bar");
+    let loading_text = document.getElementById("loading_text")
+    bar.style.width = 0
+    for (let i = 0; i < 6; i++) {
+        setTimeout(function() {
+            if (i == 5) {
+                callback();
+            } else {
+                bar.style.width = `${(i+1)*20}%`;
+                loading_text.innerText = bar_text[i];
+            }
+
+        }, 1500 * i)
+    }
+
+}
+
+
+const bars = document.querySelectorAll('.bar');
+const progress = document.querySelectorAll('.progress');
+
+bars.forEach((bar, index) => {
+    const randomWidth = Math.floor((Math.random() * 65) + 10);
+    bar.style.width = `${randomWidth}%`;
+
+    progress[index].addEventListener('mouseover', () => {
+        const randomTiming = Math.floor((Math.random() * 2) + 2);
+        console.log(randomTiming);
+        bar.style.transitionDuration = `${randomTiming}s`;
+        bar.style.width = '100%';
+    });
+})

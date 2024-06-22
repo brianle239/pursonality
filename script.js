03dd8cfb1e14f68939ea31190c738adc2a5f2cfa
Brian Le <brianle239@gmail.com>
2024-06-21 17:31:36 -0700

    added music

--- script.js
+++ script.js
@@ -15,9 +15,52 @@ async function updateContent() {
     const targetElement = document.getElementById("main_body");
 
     // Replace content
-    console.log(targetElement.innerHTML)
+
     targetElement.replaceWith(newFragment.body.firstChild)
         // targetElement.innerHTML = newFragment.body.firstChild; // Get first child (paragraph)
         // console.log(newFragment.body.firstChild)
         // console.log(newFragment.innerHTML)
+}
+
+
+
+// const audio2 = document.getElementById("cat_music2")
+// audio2.load()
+// audio2.addEventListener("ended", function() {
+//     playRandomMusic()
+// })
+
+
+
+let audio_list = []
+let element = null
+
+function playRandomMusic() {
+    random_int = Math.floor(Math.random() * audio_list.length)
+    audio = audio_list[random_int]
+    audio.play();
+    element.removeEventListener("click", playRandomMusic);
+}
+
+window.onload = function() {
+
+    const audio1 = document.getElementById("cat_music1")
+    audio1.addEventListener("ended", function() {
+        playRandomMusic()
+    })
+    audio_list.push(audio1)
+    const audio2 = document.getElementById("cat_music2")
+    audio2.load()
+    audio2.addEventListener("ended", function() {
+        playRandomMusic()
+    })
+    audio_list.push(audio2)
+    const audio3 = document.getElementById("cat_music3")
+    audio3.load()
+    audio3.addEventListener("ended", function() {
+        playRandomMusic()
+    })
+    audio_list.push(audio3)
+    element = document.getElementById("main");
+    element.addEventListener("click", playRandomMusic);
 }
